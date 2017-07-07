1bfa9b469712d413ea6f5a852f72530dcc2e9126
Jake Rushing <jakester0812@gmail.com>
2017-07-06 18:47:56 -0700

    first commit of soundboard

---
+++ YourCustomSoundboard/app/src/main/java/com/example/jake/yourcustomsoundboard/MainSoundboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jake.yourcustomsoundboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSoundboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
