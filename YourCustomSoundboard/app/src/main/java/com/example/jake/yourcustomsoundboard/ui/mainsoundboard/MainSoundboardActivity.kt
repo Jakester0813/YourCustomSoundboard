85cb6a6d50763ef9409cbfeae03a9cb5c18bb6f7
Jake Rushing <jakester0812@gmail.com>
2018-01-25 21:45:11 -0800

    Added menu items

--- YourCustomSoundboard/app/src/main/java/com/example/jake/yourcustomsoundboard/ui/mainsoundboard/MainSoundboardActivity.kt
+++ YourCustomSoundboard/app/src/main/java/com/example/jake/yourcustomsoundboard/ui/mainsoundboard/MainSoundboardActivity.kt
@@ -1,12 +1,36 @@
 package com.example.jake.yourcustomsoundboard
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.jake.yourcustomsoundboard.ui.help.HelpActivity
 
 class MainSoundboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.main_menu,menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.action_settings){
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        else if (item?.itemId == R.id.action_help){
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
