704794a641a72da3476cc4da2ff88a91b6b5eff0
Jake <jakester0812@gmail.com>
2018-01-28 20:33:21 -0800

    Started working on Settings, MainSoundboardActivity, and implemented Help Screen

--- YourCustomSoundboard/app/src/main/java/com/example/jake/yourcustomsoundboard/ui/mainsoundboard/MainSoundboardActivity.kt
+++ YourCustomSoundboard/app/src/main/java/com/example/jake/yourcustomsoundboard/ui/mainsoundboard/MainSoundboardActivity.kt
@@ -3,15 +3,30 @@ package com.example.jake.yourcustomsoundboard
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.jake.yourcustomsoundboard.ui.help.HelpActivity
+import com.example.jake.yourcustomsoundboard.util.EqualSpacingItemDecoration
 
 class MainSoundboardActivity : AppCompatActivity() {
+
+    @BindView(R.id.rv_sounds)
+    var mSoundsRecycler : RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+        mSoundsRecycler?.layoutManager = GridLayoutManager(this, 3)
+        mSoundsRecycler?.addItemDecoration(EqualSpacingItemDecoration(16))
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
---
+++ YourCustomSoundboard/app/src/main/java/com/example/jake/yourcustomsoundboard/ui/mainsoundboard/SoundboardAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.jake.yourcustomsoundboard.ui.mainsoundboard
+
+import android.support.v7.widget.RecyclerView
+
+/**
+ * Created by Jake on 1/28/2018.
+ */
+class SoundboardAdapter constructor() : RecyclerView.Adapter<>{
+
+
+
+
+
+    class SoundViewHolder constructor(View v) : RecyclerView.ViewHolder{
+
+    }
+
+}
